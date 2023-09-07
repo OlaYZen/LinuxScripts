00c219ec0171adddfd6b3a955a9007cb4e5eef72
Olai <94807067+OlaYZen@users.noreply.github.com>
2023-09-07 09:30:19 +0200

    Update archupgrade.sh

--- archupgrade.sh
+++ archupgrade.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+echo -e "\033[1K"
 echo "Installing upgrades from the official Arch Repo"
+echo -e "\033[1K"
 yes | sudo pacman -Syyu
-echo "Installation complete"
+echo -e "\033[1K"
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+echo -e "${GREEN} Installation complete"
+echo -e "${NOCOLOR}"
