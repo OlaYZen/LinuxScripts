1fd762b8e08f4350df4f95a74fcdb9a07c341bae
Olai <94807067+OlaYZen@users.noreply.github.com>
2023-09-07 09:30:20 +0200

    Update ubuntuupgrade.sh

--- ubuntuupgrade.sh
+++ ubuntuupgrade.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+echo -e "\033[1K"
 echo "Installing upgrades from the official Ubuntu Repo"
+echo -e "\033[1K"
 apt-get update && apt upgrade -y && apt install nano -y
-echo "Installation complete"
+echo -e "\033[1K"
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+echo -e "${GREEN} Installation complete"
+echo -e "${NOCOLOR}"
