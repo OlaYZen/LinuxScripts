fe8ffd4073c578583d42c1c402b96282fda2329a
Olai <94807067+OlaYZen@users.noreply.github.com>
2023-09-07 10:06:59 +0200

    Update ubuntuupgrade.sh

--- ubuntuupgrade.sh
+++ ubuntuupgrade.sh
@@ -2,11 +2,17 @@
 ORANGE='\033[0;33m'
 WHITE='\033[1;37m'
 GREEN='\033[1;32m'
+RED='\033[0;31m'
 NOCOLOR='\033[0m'
 
 echo -e "\033[1K"
 echo -e "${WHITE}Sudo is required for the installation${NOCOLOR}"
-sudo su
+sudo -v
+if [ $? -eq 0 ]; then
+    exit
+else
+    echo -e "${RED}Incorrect password${NOCOLOR}"
+fi
 echo -e "\033[1K"
 clear
 
